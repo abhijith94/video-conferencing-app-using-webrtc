d8f8b189ae063d0b9c5600060d8bb2af7631fbf6
abhijith94 <abhijith.developer@gmail.com>
2020-08-04 04:02:53 +0530

    changed config links from http to https

--- client/src/config.js
+++ client/src/config.js
@@ -1,16 +1,16 @@
 export const SERVER_URL =
   process.env.NODE_ENV === 'development'
     ? 'http://localhost:3001'
-    : 'http://abhijith.xyz/api';
+    : 'https://abhijith.xyz/api';
 
 export const SOCKET_URL =
   process.env.NODE_ENV === 'development'
     ? 'http://localhost:3002'
-    : 'http://abhijith.xyz/socket';
+    : 'https://abhijith.xyz/socket';
 
 export const FETCH_MEET_URL = SERVER_URL + '/createMeetUrl';
 
 export const MEET_BASE_URL =
   process.env.NODE_ENV === 'development'
     ? 'http://localhost:3000'
-    : 'http://abhijith.xyz';
+    : 'https://abhijith.xyz';
