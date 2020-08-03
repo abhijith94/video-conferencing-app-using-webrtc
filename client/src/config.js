3c956e72c743b367b6efe99de26adace6db46dd3
abhijith94 <abhijith.developer@gmail.com>
2020-08-04 03:40:40 +0530

    url changes

--- client/src/config.js
+++ client/src/config.js
@@ -1,10 +1,16 @@
 export const SERVER_URL =
-  process.env.NODE_ENV === 'development' ? 'http://localhost:3001' : '';
+  process.env.NODE_ENV === 'development'
+    ? 'http://localhost:3001'
+    : 'http://abhijith.xyz/api';
 
 export const SOCKET_URL =
-  process.env.NODE_ENV === 'development' ? 'http://localhost:3002' : '';
+  process.env.NODE_ENV === 'development'
+    ? 'http://localhost:3002'
+    : 'http://abhijith.xyz/socket';
 
 export const FETCH_MEET_URL = SERVER_URL + '/createMeetUrl';
 
 export const MEET_BASE_URL =
-  process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : '';
+  process.env.NODE_ENV === 'development'
+    ? 'http://localhost:3000'
+    : 'http://abhijith.xyz';
